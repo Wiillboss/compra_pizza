62dd325b8c5269dfcf6b8202b7fcfc65cdf1772b
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-03-22 17:45:02 -0300

    atualização

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let cart = [];
 let modalQt = 1;
 let modalKey = 0;
 
-const c = (el) =>  document.querySelector(el);
+const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
 
@@ -15,8 +15,8 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    
-    pizzaItem.querySelector('a').addEventListener('click', (e) =>{
+
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
@@ -27,10 +27,10 @@ pizzaJson.map((item, index) => {
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
         c('.pizzaInfo--size.selected').classList.remove('selected');
-        cs('.pizzaInfo--size').forEach((size, sizeIndex) =>{
-            if(sizeIndex == 2){
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
                 size.classList.add('selected');
-            }            
+            }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
 
@@ -38,51 +38,66 @@ pizzaJson.map((item, index) => {
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(() =>{
+        setTimeout(() => {
             c('.pizzaWindowArea').style.opacity = 1;
         }, 200);
-        
+
     });
-    
-    
-    c('.pizza-area').append( pizzaItem) ;
+
+
+    c('.pizza-area').append(pizzaItem);
 });
 
 // Eventos do Modal
 function closeModal() {
     c('.pizzaWindowArea').style.opacity = 0;
-    setTimeout(() =>{
+    setTimeout(() => {
         c('.pizzaWindowArea').style.display = 'none';
     }, 500);
 }
 
-cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) =>{
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 });
-c('.pizzaInfo--qtmenos').addEventListener('click', () =>{
-    if(modalQt>1){
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
         modalQt--;
         c('.pizzaInfo--qt').innerHTML = modalQt;
     }
 });
-c('.pizzaInfo--qtmais').addEventListener('click', () =>{
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
     modalQt++;
-    c('.pizzaInfo--qt').innerHTML = modalQt; 
+    c('.pizzaInfo--qt').innerHTML = modalQt;
 });
-cs('.pizzaInfo--size').forEach((size, sizeIndex) =>{
-    size.addEventListener('click', (e) =>{
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
         c('.pizzaInfo--size.selected').classList.remove('selected');
-        size.classList.add('selected');     
+        size.classList.add('selected');
     });
 });
 
-c('.pizzaInfo--addButton').addEventListener('click', () =>{
+c('.pizzaInfo--addButton').addEventListener('click', () => {
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
-   
-   cart.push({
-        id:pizzaJson[modalKey].id,
-        size,
-        qt:modalQt
-   });
-   closeModal();
+
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+
+    let key = cart.findIndex((item) => item.identifier = identifier);
+    /* O codigo abaixo é o mesmo que o que está acima, entretanto o se cima está mais resumido
+    let key = cart.findIndex((item) =>{
+        return item.identifier = identifier
+    });
+    */
+
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
+
+    closeModal();
 });
